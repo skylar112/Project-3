ad8d414c1071142da117c6dc219467c0490486b3
Willie Gomez <willie.gomezjr12@gmail.com>
2020-08-29 12:00:02 -0400

    updatess

--- usedcarfinder/routes/api/cars.js
+++ usedcarfinder/routes/api/cars.js
@@ -1,10 +1,10 @@
 const router = require("express").Router();
 const carsController = require("../../controllers/carsController");
 
-// Matches with "/api/books"
+
 router.route("/").get(carsController.findAll).post(carsController.create);
 
-// Matches with "/api/books/:id"
+
 router
   .route("/:id")
   .get(carsController.findById)
