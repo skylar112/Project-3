32bfb63caf14300c20e18a5dc7a606ddaff025b7
Willie Gomez <willie.gomezjr12@gmail.com>
2020-08-29 12:04:20 -0400

    updatesssss

--- usedcarfinder/client/src/utils/API.js
+++ usedcarfinder/client/src/utils/API.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 
 export default {
-  // Gets all books
+  
   getCars: function ({ userId }) {
     return axios.get("/api/cars?userId=" + userId);
   },
-  // Gets the book with the given id
+  
   getCar: function (id) {
     return axios.get("/api/cars/" + id);
   },
@@ -14,11 +14,11 @@ export default {
       ...car,
     });
   },
-  // Deletes the book with the given id
+
   deleteCar: function (id) {
     return axios.delete("/api/cars/" + id);
   },
-  // Saves a book to the database
+ 
   saveCar: function (carData) {
     return axios.post("/api/cars", carData);
   },
--- usedcarfinder/routes/api/index.js
+++ usedcarfinder/routes/api/index.js
@@ -3,7 +3,7 @@ const router = require("express").Router();
 const carRoutes = require("./cars");
 
 
-// Book routes
+
 router.use("/cars", carRoutes);
 console.log("hello2")
 module.exports = router;
