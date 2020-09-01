e9d7623b930a21a76dc97eb3f908c122e3c1667f
Kyle Cooper <kcooper89.kc@gmail.com>
2020-09-01 18:10:34 -0400

    update 5

--- usedcarfinder/client/src/components/DeleteBtn/index.js
+++ usedcarfinder/client/src/components/DeleteBtn/index.js
@@ -1,12 +1,15 @@
 import React from "react";
 import "./style.css";
 
-
 function DeleteBtn(props) {
-
   return (
-    <button className="delete-btn btn btn-danger" {...props} role="button" tabIndex="0">
-     Delete
+    <button
+      className="delete-btn btn btn-danger"
+      {...props}
+      role="button"
+      tabIndex="0"
+    >
+      Delete
     </button>
   );
 }
