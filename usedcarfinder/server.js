3856bfd0f55fa93667d2dcb1554b54a7da6a0646
Kyle Cooper <kcooper89.kc@gmail.com>
2020-09-01 17:57:46 -0400

    update: fixing comments and formatting code

--- usedcarfinder/server.js
+++ usedcarfinder/server.js
@@ -3,11 +3,12 @@ const express = require("express");
 const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
-const PORT = process.env.PORT ||5000;
+const PORT = process.env.PORT || 5000;
 
 // Configure body parsing for AJAX requests
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
 // Serve up static assets
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
@@ -17,10 +18,14 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://heroku_rcjv3c59:5om5gkjstfj3vgj95qp2cugbv8@ds121183.mlab.com:21183/heroku_rcjv3c59", {
-  useCreateIndex: true,
-  useNewUrlParser: true,
-});
+mongoose.connect(
+  process.env.MONGODB_URI ||
+    "mongodb://heroku_rcjv3c59:5om5gkjstfj3vgj95qp2cugbv8@ds121183.mlab.com:21183/heroku_rcjv3c59",
+  {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+  }
+);
 
 // Start the API server
 app.listen(PORT, () =>
