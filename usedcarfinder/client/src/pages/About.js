5eaff9060f471e46d702c0965dd15f70a86695b9
Willie Gomez <willie.gomezjr12@gmail.com>
2020-09-01 18:45:21 -0400

    about update

--- usedcarfinder/client/src/pages/About.js
+++ usedcarfinder/client/src/pages/About.js
@@ -1,10 +1,18 @@
 import React, { Component } from "react";
 import { Redirect } from "react-router-dom";
 import "../App.css";
-import Modal from 'react-modal';
+import Modal from "react-modal";
 
-
-
+const customStyles = {
+  content: {
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -100%)",
+  },
+};
 
 class About extends Component {
   state = { isSignedIn: false, isModalOpen: true };
@@ -12,29 +20,33 @@ class About extends Component {
   render() {
     return (
       <div className="App">
-      
-            <div className="Modal">
-
-              <Modal
-                isOpen={this.state.isModalOpen}
-              // onAfterOpen={afterOpenModal}
-              // onRequestClose={closeModal}
-              // style={customStyles}
-              // contentLabel="Example Modal"
-              >
-                <div class="modal-header" >
-                  <h5>How to use our page</h5>
-                 </div>
-                 
-                 <div class="modal-body">
-                  <p>hello world</p>
-                  </div>
-                
-                <div class="modal-footer">
-                  <button className="btn btn-primary" onClick={() => this.setState({ isModalOpen: false })}>CLOSE</button>
-                </div>
-              </Modal>
+        <div className="Modal">
+          <Modal
+            isOpen={this.state.isModalOpen}
+            // onAfterOpen={afterOpenModal}
+            // onRequestClose={closeModal}
+            style={customStyles}
+            // contentLabel="Example Modal"
+          >
+            <div class="modal-header">
+              <h5>How to use our page</h5>
             </div>
+
+            <div class="modal-body">
+              <h2>WELCOME</h2>
+              <p>Welcome to React Cars  </p>
+            </div>
+
+            <div class="modal-footer">
+              <button
+                className="btn btn-primary"
+                onClick={() => this.setState({ isModalOpen: false })}
+              >
+                CLOSE
+              </button>
+            </div>
+          </Modal>
+        </div>
       </div>
     );
   }
