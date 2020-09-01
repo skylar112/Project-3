6e366ad70e9f4a06f9dab575ebbf784ad6831e3b
rafael <rafquadros22@gmail.com>
2020-09-01 18:41:53 -0400

    modal sizing

--- usedcarfinder/client/src/pages/Login.js
+++ usedcarfinder/client/src/pages/Login.js
@@ -12,7 +12,7 @@ const customStyles = {
     right                 : 'auto',
     bottom                : 'auto',
     marginRight           : '-50%',
-    transform             : 'translate(-50%, -50%)'
+    transform             : 'translate(-50%, -100%)'
   }
 };
 
