0073c9d7c8f7c82c5f801438c6223e070ae092c4
rafael <rafquadros22@gmail.com>
2020-09-03 13:34:29 -0400

    login fix

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -30,8 +30,8 @@ class Login extends Component {
     signInFlow: "popup",
     signInOptions: [
       firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-      firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-      firebase.auth.GithubAuthProvider.PROVIDER_ID,
+      // firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+      // firebase.auth.GithubAuthProvider.PROVIDER_ID,
       firebase.auth.EmailAuthProvider.PROVIDER_ID,
     ],
     callbacks: {
