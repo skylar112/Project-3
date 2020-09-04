f0f3cd5b475e968496749536bdde6519c8be4d6e
rafael <rafquadros22@gmail.com>
2020-09-03 21:08:11 -0400

    lets see

---
+++ components/Nav/index.js
@@ -0,0 +1,62 @@
+import React from "react";
+import firebase from "./node_modules/firebase";
+import "./style.css";
+
+function Nav({ setOpen }) {
+  const [isSignedIn, setSignInStatus] = React.useState(false);
+  React.useEffect(() => {
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        setSignInStatus(true);
+      } else {
+        setSignInStatus(false);
+      }
+    });
+  }, []);
+  return (
+    <nav className="navbar navbar-light bg-dark">
+   
+   
+      <a className="navbar-brand text-light" href="/">
+        <h3>React Cars</h3>
+      </a>
+
+   
+      <a className="navbar-brand text-light" href="#newCars">
+        New Cars
+      </a>
+
+      <a className="navbar-brand text-light" href="#usedCars">
+        Used Cars
+      </a>
+      
+
+      {/* <a className="navbar-brand text-light" href="#about">
+        About
+      </a> */}
+
+      <a className="navbar-brand text-light" href="#video">
+        Videos
+      </a>
+
+      {isSignedIn ? (
+        <a
+          className="navbar-brand text-light"
+          href="#"
+          onClick={() => firebase.auth().signOut()}
+        >
+          Logout
+        </a>
+      ) : (
+        <a
+          className="navbar-brand  text-light"
+          href="#login"
+          onClick={() => setOpen(true)}
+        >
+          Login
+        </a>
+      )}
+    </nav>
+  );
+}
+export default Nav;
